417aa56c718c36d70880ee5be42e548d4562e45e
assur191 <cuoredoro14@gmail.com>
2014-10-27 20:55:18 -0400

    added jquery interactivity

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,35 @@
-console.log('\'Allo \'Allo!');
+$('document').ready(function() {
+
+    var button = function(value){
+        return  '<li class="item">' +
+                '<div class="button btn-todo">' + value + '</div>' +
+                '<img src="images/delete_16.png" class="remove">' +
+                '</li>';
+    };
+
+    $(document).on('click', '.plus', function(){
+        addButton($('.item-input').val());
+    });
+
+    $(document).on('keypress', '.item-input',function(){
+        if (event.which == 13) {
+            addButton($('.item-input').val());
+        }
+    });
+
+    $(document).on('click', '.remove', function(){
+        $(this).parent().remove()
+    });
+
+    $(document).on('click', '.button',function(){
+        $(this).toggleClass('btn-todo');
+        $(this).toggleClass('btn-done');
+    });
+
+    function addButton(value){
+        if(value !== ''){
+            $('.list').append(button(value));
+            $('.item-input').val('');
+        }
+    }
+});
